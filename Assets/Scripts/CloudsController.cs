9c0910f5e67682a6f62d0d1d4c6cca0765bd0316
Chris Sim <monkeymon@gmail.com>
2014-08-24 22:50:04 +0800

    Adding Clouds into main play scene
    Signed-off-by: Chris Sim <monkeymon@gmail.com>

---
+++ Assets/Scripts/CloudsController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudsController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector2 velocity = new Vector2(Random.Range(-4.0F, 0F), 0);
+		rigidbody2D.velocity = velocity;
+		transform.position = new Vector3(transform.position.x, transform.position.y - 2 * Random.value, transform.position.z);
+	}
+}
--- Assets/Scripts/GenerateController.cs
+++ Assets/Scripts/GenerateController.cs
@@ -4,9 +4,19 @@ using System.Collections;
 public class GenerateController : MonoBehaviour {
 
 	public GameObject Rocks;
+
+	public GameObject[] Clouds;
 	// Use this for initialization
 	void Start () {
 		InvokeRepeating ("createRock", 1f, 1.5f);
+		InvokeRepeating ("createCloud", 0.5f, 1.5f);
+	}
+
+	void createCloud() {
+		int random = Random.Range (0, Clouds.Length - 1);
+		Object createdCloud = Instantiate (Clouds[random]);
+		DestroyObject (createdCloud,10f);
+
 	}
 
 	void createRock() {
